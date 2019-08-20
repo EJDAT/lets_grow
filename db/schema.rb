a8e6dc8fa2dc0424ab1f06015272fedc3e848bdf
Alexandra Vanhalme <alexandravanhalme@hotmail.com>
2019-08-20 14:50:36 +0200

    seeding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_183704) do
+ActiveRecord::Schema.define(version: 2019_08_20_114252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_183704) do
   end
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
     t.string "description"
     t.date "date"
     t.boolean "validated"
