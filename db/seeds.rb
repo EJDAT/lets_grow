dcf476570e7c97187d816e25b1381b3faa00b9c8
Maxime Rieder <maximerieder@yahoo.com>
2019-08-22 15:52:29 +0200

    user can add plant to garden

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@ User.destroy_all
 
 puts "Creating Users"
 
-User.create!(email: "max@gmail.com", password: "123456")
-User.create!(email: "alex@gmail.com", password: "123456")
-User.create!(email: "ejdat@gmail.com", password: "123456")
+User.create!(id: 1, email: "max@gmail.com", password: "123456")
+User.create!(id: 2, email: "alex@gmail.com", password: "123456")
+User.create!(id: 3, email: "ejdat@gmail.com", password: "123456")
 
 puts "Gardens were created automatically"
 
@@ -21,7 +21,8 @@ plants_list = [
     "Beets, Beta vulgaris, are herbacious biennial root vegetables in the family Chenopodiaceae grown for their edible root. The plant is usually erect with a long main root and a rosette of leaves growing on stems. The leaves are oval in shape, arranged alternately on the stem and grow 20–40 cm (7.9–15.7 in) in length. The roots are usually red in color. The plant produces sessile green flowers and can reach 1–2 m (3.3–6.6 ft) in height. Beets are usually grown as annual plants, harvested after one growing season. Beets may also be referred to as beetroot, garden beet or spinach beet and originated from the Mediterranean.",
     2,
     "End May till mid July",
-    "August till October"
+    "August till October",
+    "101-512_d2wijr.png"
     ],
 
 
@@ -31,7 +32,8 @@ plants_list = [
     "La tomate cerise est l’un des fruits/légumes, les plus faciles à cultiver car elles poussent rapidement, peuvent se cultiver aussi bien en terre qu’en pot sur un balcon ou une terrasse et peuvent être récoltées tout au long de l’été au gré des besoins.",
     4,
     "From mi May",
-    "From July till end of September"
+    "From July till end of September",
+    "tomatoes-512_njqywf.png"
   ],
 
   [ 3,
@@ -40,68 +42,65 @@ plants_list = [
     "The cucumber is a creeping vine that roots in the ground and grows up trellises or other supporting frames, wrapping around supports with thin, spiraling tendrils.[1] The plant may also root in a soilless medium and will sprawl along the ground if it does not have supports. The vine has large leaves that form a canopy over the fruits. The fruit of typical cultivars of cucumber is roughly cylindrical, but elongated with tapered ends, and may be as large as 60 centimeters (24 in) long and 10 centimeters (3.9 in) in diameter.[citation needed] Botanically speaking, the cucumber is classified as a pepo, a type of botanical berry with a hard outer rind and no internal divisions. Much like tomato and squash, it is often perceived, prepared and eaten as a vegetable. Cucumber fruits consist of 95% water (see nutrition table).",
     2,
     "June",
-
-    "August till mid October"
+    "August till mid October",
+    "vegetable_colour-15-512_yjbukp.png"
     ],
   [ 5,
     "1537988634077-c99a6f91e7b5_xpax06.jpeg",
-
     "Chilli Pepper",
     "Tous les piments sont originaires du sud de l’Amérique du Nord, d’Amérique centrale, et d’Amérique du Sud. Ramené en Europe par Christophe Colomb, le piment, brulant à l’origine, a été sélectionné afin d’obtenir des variétés de plus en plus douces : les poivrons. De leur côté, les piments brulants, utilisés comme condiments se répandent sur tout le globe et se diversifient également en variétés, des plus douces aux plus piquantes.",
     5,
     "Februar till May",
-    "July till October" ],
+    "July till October",
+    "hot-pepper-512_yx5jyr chilli pepper.png"
+    ],
 
   [ 6,
     "slack-imgs.com_e6r9xj.jpeg",
-
     "Fennel",
     "(Foeniculum vulgare), perennial herb of the carrot family (Apiaceae) grown for its edible shoots, leaves, and seeds. Native to southern Europe and Asia Minor, fennel is cultivated in temperate regions worldwide and is considered an invasive species in Australia and parts of the United States. All parts of the plant are aromatic and used in flavouring, and the bulblike stem base of Florence fennel (variety azoricum) and the blanched shoots are eaten as a vegetable. The seeds and extracted oil are suggestive of anise in aroma and taste and are used for scenting soaps and perfumes and for flavouring candies, liqueurs, medicines, and foods, particularly pastries, sweet pickles, and fish.",
     2,
     "June till mid. july",
-
-    "July till beginning October"
+    "July till beginning October",
+    "fennel_gisesm.png"
     ],
   [ 7,
     "1558717738-12f5dde036a7_gm8pqi.jpeg",
-
     "Leek",
     "Is a biennial garden herb (Allium porrum synonym A. ampeloprasum porrum) of the lily family. Leeks, which are native to Mediterranean countries, have been a popular food as recorded through a history of food stretching back more than 4,000 years in the Middle East. They also are nutritious, being a rich source of vitamin C, a good source of folate (vitamin B9), and a source of iron. The leek has also been a national symbol for the Welsh.",
     1,
     "May till mid. June",
-
-    "September till end December"
+    "September till end December",
+    "vegetable_colour-11-512_txltym.png"
     ],
 
   [ 8,
     "1474591424615-7fe467c7fac9_tqku65.jpeg",
-    "Pumkin",
+    "Pumpkin",
     "Pumpkin belongs to the family Cucurbitaceae and is grown primarily as a vegetable or ornamental plant. Pumpkin plants are short lived annual or perennial vines with branching tendrils and broad lobed leaves. The plant produces large yellow or orange flowers and a pepo fruit (berry with a thick rind) known as a pumpkin. The fruit can range greatly in size, from miniature pumpkins weighing a few ounces to giant pumpkins which can reach over 75 lbs (34 kg). The skin of the pumpkin is usually ribbed and is usually orange on color although some varieties are green, grey, yellow or red in color. Pumpkin plants are usually grown as annuals, surviving one growing season and the vines are capable of reaching 15 m (50 ft) in length if vines are allowed to root. ",
     4,
     "June",
-
-    "mid. September till mid. October"
+    "mid. September till mid. October",
+    "f80f98449ed9cc2a90b03504a249ebca-pumpkin-icon-flat-by-vexels_ihxxud.png"
     ],
   [ 9,
     "1495026206962-e8b160431ffe_ye4i74.jpeg",
-
     "Zucchini",
     "As implied by the term summer squash, zucchini’s prime season is May to August, although they are now available year-round. Summer squash has a thin, soft skin and soft edible seeds, whereas winter squash has a hard skin.
     The zucchini is a long, cylindrical vegetable, slightly smaller at the stem end, usually dark green in color. The flesh is a pale greenish-white and has a delicate, almost sweet flavor. Zucchini fruit grows quickly and is harvested within 2 to 7 days of flowering. Over-ripe zucchini and those past their prime can be bitter.",
     4,
     "May mid. June",
-
-    "August till beginning October"
+    "August till beginning October",
+    "zucchini-8-236670_hl31yc.png"
     ],
   [ 10,
     "1535915190037-fe793850a5cb_i8pnfw.jpeg",
-
     "Pepper",
     "Pepper is a tender, warm-season vegetable. Pepper plants require somewhat higher temperatures, grow more slowly and are smaller than most tomato plants. Brightly colored, sweet bell pepper varieties have recently burst onto the scene. A vast range of other garden peppers (pimiento, tabasco, cayenne, chili and paprika) may be grown for food, spices or as ornamentals.",
     3,
     "May till mid. June",
-
-    "Mid. July till mid. October"
+    "Mid. July till mid. October",
+    "135701_tgtimr.svg"
     ],
 
   [ 11,
@@ -110,8 +109,8 @@ plants_list = [
     "The garden strawberry (or simply strawberry; Fragaria × ananassa)[1] is a widely grown hybrid species of the genus Fragaria, collectively known as the strawberries, which are cultivated worldwide for their fruit. The fruit is widely appreciated for its characteristic aroma, bright red color, juicy texture, and sweetness. It is consumed in large quantities, either fresh or in such prepared foods as jam, juice, pies, ice cream, milkshakes, and chocolates. Artificial strawberry flavorings and aromas are also widely used in products such as candy, soap, lip gloss, perfume, and many others.",
     4,
     "Mid. September till end of October.",
-
-    "June till mid July."
+    "June till mid July.",
+    "Organic_fresh_food_fruits_berries_strawberry-512_d6crra.png"
     ],
 
   [ 12,
@@ -120,28 +119,26 @@ plants_list = [
     "annual leaf vegetable of the aster family (Asteraceae). Most lettuce varieties are eaten fresh and are commonly served as the base of green salads. Lettuce is generally a rich source of vitamins K and A, though the nutritional quality varies, depending on the variety.",
     1,
     "April till mid. September",
-
-    "Mid. May till end October"
+    "Mid. May till end October",
+    "vegetable_food_colour-02-512_tyqbwp.png"
     ],
   [ 13,
     "1510627498534-cf7e9002facc_qcjthg.jpeg",
-
     "Cauliflower",
     "Cauliflower is one of several vegetables in the species Brassica oleracea in the genus Brassica, which is in the family Brassicaceae. It is an annual plant that reproduces by seed. Typically, only the head is eaten – the edible white flesh sometimes called ‘crud’ (with a similar appearance to cheese curd). The cauliflower head is composed of a white inflorescence meristem. Cauliflower heads resemble those in broccoli, which differs in having flower buds as the edible portion. Brassica oleracea also includes broccoli, brussels sprouts, cabbage, collard greens, and kale, collectively called ‘cole’ crops,though they are of different cultivar groups.",
     2,
     "Mid. April till mid. May",
-
-    "July till mid. October"
+    "July till mid. October",
+    "1135562_hmhteq.svg"
     ],
   [ 14,
     "1528607950896-30d92379b592_bsxnkz.jpeg",
-
     "Rhubarb",
     "Rhubarb is a vegetable with a unique taste that makes it a favorite in many pies and desserts. It originated in Asia over 2,000 years ago. It was initially cultivated for its medicinal qualities, it was not until the 18th century that rhubarb was grown for culinary purposes in Britain and America. Rhubarb is often commonly mistaken to be a fruit but rhubarb is actually a close relative of garden sorrel, and is therefore a member of the vegetable family. Rhubarb is rich in vitamin C and dietary fiber.",
     3,
     "Jan till February",
-
-    "Mid. April till mid. June"
+    "Mid. April till mid. June",
+    "1135610_dmvtaw.svg"
     ],
   [ 15,
     "1472653525502-fc569e405a74_bozw2v.jpeg",
@@ -149,12 +146,13 @@ plants_list = [
     "Rhubarb is a vegetable with a unique taste that makes it a favorite in many pies and desserts. It originated in Asia over 2,000 years ago. It was initially cultivated for its medicinal qualities, it was not until the 18th century that rhubarb was grown for culinary purposes in Britain and America. Rhubarb is often commonly mistaken to be a fruit but rhubarb is actually a close relative of garden sorrel, and is therefore a member of the vegetable family. Rhubarb is rich in vitamin C and dietary fiber.",
     2,
     "March till July",
-    "July till November"
+    "July till November",
+    "imgbin-computer-icons-vegetable-carrot-vegetable-YBe3sAhhMi2qZFeE3TWiU94pT_mgxgp5.svg"
     ]
 ]
 
-plants_list.each do |id, photo_url, name, description, price, plant_date, harvest_date|
-  Plant.create!(id: id, photo_url: photo_url, name: name, description: description, price: price, plant_date: plant_date, harvest_date: harvest_date)
+plants_list.each do |id, photo_url, name, description, price, plant_date, harvest_date, logo_url|
+  Plant.create!(id: id, photo_url: photo_url, name: name, description: description, price: price, plant_date: plant_date, harvest_date: harvest_date, logo_url: logo_url)
 
 end
 
