eebfc5bed4b204e9904ed2d8496e482d0718ad11
ejdataras <ejdaras@gmail.com>
2019-08-23 15:44:31 +0200

    goes on falt pictures

---
+++ app/javascript/components/card-trip.js
@@ -0,0 +1,17 @@
+// const initDescriptionOnCardTrip = () => {
+
+//   const cardtrip = document.querySelectorAll('card-category');
+//   cardtrip.forEach((card-category) => {
+//     window.addEventListener('click', () => {
+//       if (window.scrollY >= window.innerHeight) {
+//         event.currentTarget.classList.p.add('dropdown-menu-white');
+//       } else {
+//         event.currentTarget.classList.remove('dropdown-menu-white');
+//       }
+//     });
+//   }
+// }
+
+
+
+// export { initDescriptionOnCardTrip };
