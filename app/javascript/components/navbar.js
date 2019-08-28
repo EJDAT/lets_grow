2bc2180b3016e154d8ee1353519380b28e33396a
ejdataras <ejdaras@gmail.com>
2019-08-29 00:20:37 +0200

    happy easter

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY) {
         navbar.classList.add('navbar-lewagon-white');
       } else {
         navbar.classList.remove('navbar-lewagon-white');
@@ -16,7 +16,7 @@ const initUpdateNavbarOnScrolllink = () => {
   navbar.forEach((navbar) => {
     if (navbar) {
       window.addEventListener('scroll', () => {
-        if (window.scrollY >= window.innerHeight) {
+        if (window.scrollY) {
           navbar.classList.add('nav-link-black');
         } else {
           navbar.classList.remove('nav-link-black');
@@ -32,7 +32,7 @@ const initUpdateNavbarOnScrollmenu = () => {
   const navbar = document.querySelector('.dropdown-menu');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY) {
         navbar.classList.add('dropdown-menu-white');
       } else {
         navbar.classList.remove('dropdown-menu-white');
