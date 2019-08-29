b654cf9a29f50d62e2cf46c52ce97c8abfc5f106
Alexandra Vanhalme <alexandravanhalme@hotmail.com>
2019-08-29 15:30:22 +0200

    added the fun facts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_100809) do
+ActiveRecord::Schema.define(version: 2019_08_29_130953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_100809) do
     t.jsonb "plant_date", default: {}
     t.jsonb "harvest_date", default: {}
     t.integer "difficulty"
-    t.integer "fun_fact"
+    t.string "fun_fact"
   end
 
   create_table "tasks", force: :cascade do |t|
